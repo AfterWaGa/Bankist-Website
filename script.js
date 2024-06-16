50914ecace3b5cba5e78afc43666f369c9c4399c
Roman <shelepov-rmn@mail.ru>
2024-06-17 00:08:55 +0300

    Added an operations tabs functionality

--- script.js
+++ script.js
@@ -50,3 +50,27 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
         document.querySelector(id).scrollIntoView({ behavior: "smooth" });
     }
 });
+
+// Operations tabs
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+    const clicked = e.target.closest(".operations__tab");
+
+    // Защита от клика вне кнопки
+    if (!clicked) return;
+
+    tabs.forEach(function (tab) {
+        return tab.classList.remove("operations__tab--active");
+    });
+    clicked.classList.add("operations__tab--active");
+
+    // Показ контента таба
+    tabsContent.forEach(function (content) {
+        return content.classList.remove("operations__content--active");
+    });
+
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add("operations__content--active");
+});
