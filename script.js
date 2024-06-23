7e12f9a211a7553c4380d4a0a3fce7426d754eee
Roman <shelepov-rmn@mail.ru>
2024-06-23 14:45:51 +0300

    Added sticky nav

--- script.js
+++ script.js
@@ -92,5 +92,21 @@ const handleHover = function (e) {
 };
 
 nav.addEventListener("mouseover", handleHover.bind(0.5));
-
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+    const [entry] = entries; // entries[0]
+
+    if (!entry.isIntersecting) nav.classList.add("sticky");
+    else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
